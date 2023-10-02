1f0b5a2897d65b67ef325d1963dc93582eb39bd8
ankit <ankitkuswaha07@gmail.com>
2023-10-02 17:27:10 +0530

    first commit

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ config({
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  // origin: [process.env.FRONTEND_URL],
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true,
 }))
